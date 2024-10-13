bf3cbaa20ac03ee681b7d50c3fda6433ab4028c1
MOSTAFA <rawanahmed681@gmail.com>
2024-10-14 02:45:03 +0300

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./module/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  {path: 'contuct' , component: FooterComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./module/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
